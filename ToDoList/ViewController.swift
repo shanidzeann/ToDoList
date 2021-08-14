3b6b67a091c5299146f2f8d5e61f7a42a7f4755b
shanidzeann <shanidzeann@mail.ru>
2021-08-14 20:36:39 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Анна Шанидзе on 14.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
